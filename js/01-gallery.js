81f59439e3a7c1a6aaf0b7d6c5a634e6dd477881
Tatiana Bidnenko <tnbdnnk@gmail.com>
2023-03-29 15:31:24 +0300

    hw7js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', handleImgClick);
+
+function handleImgClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modal = basicLightbox.create(` <img class="gallery__image" src="${e.target.dataset.source}"/> `);
+    modal.show();
+
+    document.addEventListener('keydown', escModal);
+    function escModal({code}) {
+        if (code === `Escape`) {
+            modal.close();
+        }
+    }
+}
+
+const makeGalleryGrid = ({ preview, original, description }) => 
+    `<div class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`;
+
+const markupGallery = galleryItems.map(e => makeGalleryGrid(e)).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', markupGallery);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const makeGalleryGrid = ({ preview, original, description }) => 
+    `<li class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+
+const markupGallery = galleryItems.map(e => makeGalleryGrid(e)).join('');
+galleryContainer.insertAdjacentHTML('afterbegin', markupGallery);
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
